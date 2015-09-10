54d5aee7c5efd82620568f20610ccaba3e620ef7
iYonatan <yonatan.eilat@gmail.com>
2015-09-10 20:58:30 +0300

    ughj

--- db_connector.py
+++ db_connector.py
@@ -37,7 +37,35 @@ class connector:
             arr.append(row)
         self.cnx.commit()
         cursor.close()
-        print arr
         return exist
+
+    '''
+        what2set is a function that tells you what command to give to the remote database: INSERT, UPDATE, DELETE.
+
+        ** The Function what2set doesnt take the mountpoints array!!!
+    '''
+
+    def what2set(self,request,data,devices_arr=[]):
+        exist = False
+        need2delete = []
+        exist_devices = []
+
+        cursor = self.cnx.cursor()
+        cursor.execute(request,data)
+
+        for row in cursor:
+            exist = True
+            need2delete.append(row)
+        if exist:
+            if len(devices_arr) is 0:
+                return "UPDATE"
+            exist_devices.append([i[1] for i in need2delete])
+            for device in need2delete:
+                exist_devices.append(device[1])
+            if len(list(set(exist_devices) - set(devices_arr))) > 0:
+                return "DELETE"
+        else:
+            return "INSERT"
+
     def close(self):
         self.cnx.close()
--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if not db_con.if_exist(exist,exist_data):
     cpu_data = (user_id,proc.name,proc.num_proccess)
     db_con.insert(add_cpu,cpu_data)
 
-print "CPU has been edited"
+
 
 memo = memory()
 exist = "SELECT user_id FROM memory WHERE user_id= %s"
@@ -37,21 +37,37 @@ if not db_con.if_exist(exist,exist_data):
     memo_data = (memo.total,user_id)
     db_con.insert(add_memo,memo_data)
 
-print "MEMORY has been edited"
+
 
 disk = disk()
-print disk.devices
-for d in range(len(disk.devices)):
-    add_disk = "SELECT user_id,device,mountpoint FROM disk WHERE (user_id=%s AND device=%s AND mountpoint=%s)"
-    data = (user_id,disk.devices[d],disk.devices[d])
-    if not db_con.if_exist(add_disk,data):
-         add_disk = ("INSERT INTO disk"
-                     "(user_id,os_name,device,mountpoint,fstype,total)"
-                     "VALUES (%s,%s,%s,%s,%s,%s)")
-         disk_data = (user_id,disk.os_name,disk.devices[d],disk.mountpoint[d],disk.fstype[d],disk.total[d])
-         db_con.insert(add_disk,disk_data)
-    else:
-        print
+add_disk = "SELECT user_id,device,mountpoint FROM disk WHERE user_id=%s"
+data = (user_id,)
+op = db_con.what2set(add_disk,data,disk.devices)
+
+if op is "INSERT":
+    for d in range(len(disk.devices)):
+        if not db_con.if_exist(add_disk,data):
+             add_disk = ("INSERT INTO disk"
+                         "(user_id,os_name,device,mountpoint,fstype,total)"
+                         "VALUES (%s,%s,%s,%s,%s,%s)")
+             disk_data = (user_id,disk.os_name,disk.devices[d],disk.mountpoint[d],disk.fstype[d],disk.total[d])
+             db_con.insert(add_disk,disk_data)
+
+elif op is "UPDATE":
+    print "UPDATE"
+else:
+    print "REMOVE"
+
+# for d in range(len(disk.devices)):
+#
+#     if not db_con.if_exist(add_disk,data):
+#          add_disk = ("INSERT INTO disk"
+#                      "(user_id,os_name,device,mountpoint,fstype,total)"
+#                      "VALUES (%s,%s,%s,%s,%s,%s)")
+#          disk_data = (user_id,disk.os_name,disk.devices[d],disk.mountpoint[d],disk.fstype[d],disk.total[d])
+#          db_con.insert(add_disk,disk_data)
+#     else:
+#         print
 
 print "DISK has been edited"
 
